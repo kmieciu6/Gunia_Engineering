6421384e6bc353e73634ef74701e6781f00551ce
jakub <jakub.kmiecik96@gmail.com>
2023-01-31 22:18:35 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,23 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope, faPhone} from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
     return (
-        <>
-            <h2>footer</h2>
-        </>
+        <section id='footer'>
+            <div className='phone_mail'>
+                <div>
+                    <a href="mailto:engineering.finite@gmail.com">
+                        <FontAwesomeIcon icon={faEnvelope}/>
+                        <p>engineering.finite@gmail.com</p>
+                    </a>
+                    <a href="tel:+61">
+                        <FontAwesomeIcon icon={faPhone}/>
+                        <p>+61 ??????</p>
+                    </a>
+                </div>
+            </div>
+
+        </section>
     )
 }
 
