3f5529843cdf4dd46a36a5ef52f92c9290894f8b
jakub <jakub.kmiecik96@gmail.com>
2023-01-31 23:44:06 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,30 @@ import {faEnvelope, faPhone} from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
     return (
         <section id='footer'>
-            <div className='phone_mail'>
-                <div>
+            <div>
+                <div className='phone_mail'>
                     <a href="mailto:engineering.finite@gmail.com">
-                        <FontAwesomeIcon icon={faEnvelope}/>
+                        <FontAwesomeIcon icon={faEnvelope} className='icon'/>
                         <p>engineering.finite@gmail.com</p>
                     </a>
                     <a href="tel:+61">
-                        <FontAwesomeIcon icon={faPhone}/>
-                        <p>+61 ??????</p>
+                        <FontAwesomeIcon icon={faPhone} className='icon'/>
+                        <p>+61 xxx xx xx</p>
                     </a>
                 </div>
+                <div className='copyright'>
+                    <p>
+                        Copyright © 2023
+                        <a href="https://finiteengineering.com.au"> finiteengineering.com.au</a>
+                    </p>
+                    <p>
+                        Made by
+                        <a href="https://github.com/kmieciu6?tab=repositories">
+                            <b> Jakub Kmiecik</b>
+                        </a>
+                    </p>
+                </div>
             </div>
-
         </section>
     )
 }
