cd18fabd39cecea5d936c0cbbb9118c571b5ddd8
jakub <jakub.kmiecik96@gmail.com>
2023-07-06 00:20:22 +0200

    about us

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./header/Header";
 import Footer from "./Footer";
+import photo1 from '../assets/7.jpg';
+import photo2 from '../assets/8.jpg';
 
 const About = () => {
     return (
@@ -10,19 +12,23 @@ const About = () => {
                 <div className="title">
                     <h2>About</h2>
                 </div>
-                <div>
+                <div className="text">
                     <p>
                         Welcome to Finite Engineering, a leading mechanical consultancy company dedicated to providing 
                         innovative solutions and exceptional expertise in the field of mechanical engineering including 
                         aerospace, automotive, medical and heavy industry. With a team of highly skilled professionals 
                         (NER and PhD engineers) and a client-centric approach, we offer comprehensive services to meet 
                         your specific needs and deliver superior results.
-                        <br/><br/>
+                    </p>
+                    <img src={photo1} alt='photos' className="photo"/>
+                    <p>
                         At Finite Engineering, we specialize in optimizing mechanical systems, enhancing performance, 
                         and driving innovation across a wide range of industries. With years of experience and a deep 
                         understanding of mechanical engineering principles, we are well-equipped to tackle complex 
                         challenges and provide effective solutions.
-                        <br/><br/>
+                    </p>
+                    <img src={photo2} alt='photos' className="photo"/>
+                    <p>
                         One of our key strengths is our expertise in mechanical design and analysis. Whether it's 
                         conceptualizing new products, optimizing existing systems, or performing detailed simulations, 
                         we employ advanced techniques such as finite element analysis (FEA) and computational fluid 
